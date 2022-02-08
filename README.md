75b5e032189866c4d30f0985381d4f7e0ee6abcd
Marcelo Silva <marcelos@microsoft.com>
2022-02-08 11:35:47 -0600

    Added demo to showcase mounting existing disks and file shares

--- README.md
+++ README.md
@@ -2,12 +2,30 @@
 Demo of shared volume in windows containers in AKS
 
 The idea is this demo is to show that one can have a Windows application, running in AKS in a Windows node and use a shared volume setup as an Azure Managed Disk.
-</P>
+The application is a simple IIS core server and all it does it mount the volumes created.
+
+Each volume mounted showcases one type of shared volume that can be used:
+- E: drive -> Dynamically created Azure Managed Disk
+- F: drive -> Dynamically created Azure File Share
+- G: drive -> Existing Azure File Share mounted as a volume
+- H: drive -> Existing Azure Managed Disk mounted as a volume
+
 It reuses the assets from the following articles:
 
 - [Storage options for applications in Azure Kubernetes Service (AKS)](https://docs.microsoft.com/en-us/azure/aks/concepts-storage)
 - [Create a Windows Server container on an Azure Kubernetes Service (AKS) cluster using the Azure CLI](https://docs.microsoft.com/en-us/azure/aks/windows-container-cli)
+- [Manually create and use a volume with Azure Files share in Azure Kubernetes Service (AKS)](https://docs.microsoft.com/en-us/azure/aks/azure-files-volume)
+- [Dynamically create and use a persistent volume with Azure disks in Azure Kubernetes Service (AKS)](https://docs.microsoft.com/en-us/azure/aks/azure-disks-dynamic-pv)
+- [Manually create and use a volume with Azure disks in Azure Kubernetes Service (AKS)](https://docs.microsoft.com/en-us/azure/aks/azure-disk-volume)
+- [Persistent Volume Management and Expansion in Kubernetes with Azure Kubernetes Service](https://thecloudblog.net/post/persistent-volume-management-and-expansion-in-kubernetes-with-azure-kubernetes-service/)
+- [AZ CLI Reference](https://docs.microsoft.com/en-us/cli/azure/reference-index?view=azure-cli-latest)
+- [Create an AKS cluster with managed identities](https://docs.microsoft.com/en-us/azure/aks/use-managed-identity#create-an-aks-cluster-with-managed-identities)
+
 
 To run, just run `setup.ps1` in Visual Studio Code PowerShell terminal or a PowerShell prompt.
 
-Tip: I'd recommend you execute each step individually to better understand what's going on.
+**Tip**: I'd recommend you execute each step individually to better understand what's going on.
+
+**Note**: This demo is not intended to be a production-ready demo. It is just a demo to show how to use the Azure Kubernetes Service (AKS) to deploy a Windows application.
+
+**Note**: I used az aks update to enable managed identity for the cluster since I already had that created. You could just add the switch to the az aks create command.
--- setup.ps1
+++ setup.ps1
@@ -6,6 +6,7 @@ $rg = "aks-volume-demo-rg"
 $location = "southcentralus"
 az group create --name $rg --location $location
 
+# Set up demo cluster
 $WINDOWS_USERNAME="adminuser"
 $aks_cluster_name="aks-volume-cluster1"
 az aks create `
@@ -17,7 +18,8 @@ az aks create `
     --windows-admin-username $WINDOWS_USERNAME `
     --vm-set-type VirtualMachineScaleSets `
     --kubernetes-version 1.21.7 `
-    --network-plugin azure
+    --network-plugin azure `
+    --enable-managed-identity
 
 az aks nodepool add `
     --resource-group $rg `
@@ -26,20 +28,63 @@ az aks nodepool add `
     --name npwin `
     --node-count 1
 
+# az aks update --name $aks_cluster_name --resource-group $rg --enable-managed-identity
+
 az aks get-credentials --resource-group $rg --name $aks_cluster_name
 
-# Create a volume with a size of 10GB
-# Article: https://docs.microsoft.com/en-us/azure/aks/concepts-storage
+# Create a azure file share in the same resource group as the cluster
+$share_name = "aks-volume-demo-share"
+$storage_account_name = "aksfilesharedemosa"
+az storage account create --resource-group $rg `
+                          --name $storage_account_name `
+                          --location $location `
+                          --sku Standard_LRS `
+                          --kind StorageV2 `
+                          --query id `
+                          -o tsv
+$storage_connection_string=$(az storage account show-connection-string --resource-group $rg --name $storage_account_name -o tsv)                                                           
+az storage share create --name $share_name --connection-string $storage_connection_string
+$storage_key=$(az storage account keys list --resource-group $rg --account-name $storage_account_name --query [0].value -o tsv)
 
-# Deploy persistent volume claim
-kubectl apply -f .\persistentvolclaim.yaml
+# Store storage key as secret in the cluster
+$storage_key_secret_name = "aks-volume-demo-storage-key"
+kubectl create secret generic $storage_key_secret_name --from-literal=azurestorageaccountname=$storage_account_name --from-literal=azurestorageaccountkey=$storage_key
+
+# Create a managed disk in the same resource group as the cluster
+$disk_name = "aks-volume-demo-disk"
+$disk_size = "10"
+$disk_resource_id = $(az disk create --resource-group $rg --name $disk_name --size-gb $disk_size -o tsv --query id)
+
+# Grant access to the existing disk for the cluster identity 
+$identity = $(az aks show --name $aks_cluster_name --resource-group $rg --query "identity.principalId" -o tsv)
+az role assignment create --role "Storage Account Contributor" `
+    --assignee $identity `
+    --scope $disk_resource_id 
+
+# Dyamically create disk volumes ans file shares
+kubectl apply -f .\dynamic-disk-pv-claim.yaml
+kubectl apply -f .\dynamic-share-pv-claim.yaml
+
+# Use statically created disk volumes
+kubectl apply -f .\existing-disk-storageclass.yaml
+kubectl apply -f .\existing-disk-pv.yaml
+kubectl apply -f .\existing-disk-pv-claim.yaml
+
+# Use statically created file shares
+kubectl apply -f .\existing-share-pv.yaml
+kubectl apply -f .\existing-share-pv-claim.yaml
 
 # Deploy application
 kubectl apply -f .\app.yaml
+kubectl delete -f .\app.yaml
 
-# Check application status
+# Check application status (it may take a while to start up)
+kubectl describe pod iiscorebox
 kubectl get pods
 
 # Check volumes mapped in the pod
 kubectl exec -it iiscorebox cmd.exe /C fsutil fsinfo drives
+kubectl exec -it iiscorebox cmd.exe /C dir C:\windows
+kubectl exec -it iiscorebox cmd.exe /C copy c:\windows\win.ini h:\
+kubectl exec -it iiscorebox cmd.exe /C dir h:\
 
